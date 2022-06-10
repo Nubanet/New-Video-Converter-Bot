acd71e6520efa21d8082026e93eae7f0ae88ecf2
Bikash Maity <104905898+DarkLord-99@users.noreply.github.com>
2022-06-11 01:51:52 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -1,45 +1,45 @@
 #Don't be a thief by stealing other's Hardwork, it took time and effort to make this repo.
 #Respect my work by not making any changes here.
 
-START_TEXT = "Send me any file to begin."
+START_TEXT = "Hello 👋\n\nMy Name is Converter Bot.\nSend me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/BX_Botz_Updates"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/BX_Botz_Support"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @hellodarklord\n\nWritten in python library TELETHON.\n\nBot by : @BX_Botz_Updates\nSupport : @BX_Botz_Support"   
 
-help_text = """**v1.4**
+help_text = """**Contact Support Group: BX_Botz_Support**
 
-•`Encode` - encode your video into different lib format or resolution
+•`Encode` - Encode your video into different lib format or resolution
 
-•`HEVC compress` - negligible loss compression
+•`HEVC compress` - Negligible loss compression
 
 •`FAST compress` - Very fast and Efficient compression
 
-•`Convert` - change formats or extract audio of any video
+•`Convert` - Change formats or extract audio of any video
 
-•`Rename` - rename any file, extension not required
+•`Rename` - Rename any file, extension not required
 
-•`SSHOTS` - generate 10 screenshots of your video
+•`SSHOTS` - Generate 10 screenshots of your video
 
-•`Trim` - cut your videos"""
+•`Trim` - Cut your videos"""
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/hellodarklord"
 
 spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
-JPG = "LOCAL/video_convertor.jpg"
+JPG = "https://telegra.ph/file/a9620e260d9b7dd28c35b.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/a9620e260d9b7dd28c35b.jpg'
 
-JPG2 = "LOCAL/20211215_165751.jpg"
+JPG2 = "https://telegra.ph/file/a9620e260d9b7dd28c35b.jpg"
 
-JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
+JPG3 = "https://telegra.ph/file/a9620e260d9b7dd28c35b.jpg"
 
-JPG4 = "LOCAL/20211219_000258.jpg"
+JPG4 = "https://telegra.ph/file/a9620e260d9b7dd28c35b.jpg"
 
